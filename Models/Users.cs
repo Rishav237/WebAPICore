11fff20f2361e33bca74ee036dbafc8596594d22
Rishav237 <rishav237@gmail.com>
2018-08-05 21:06:51 +0530

    User Defined CHanges

---
+++ Models/Users.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPICore.Models
+{
+    public class Users
+
+    {
+
+        public string ID { get; set; }
+
+        public string Cmp_Name { get; set; }
+
+        public string Address { get; set; }
+
+    }
+}
